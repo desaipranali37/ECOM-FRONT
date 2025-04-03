f1a67d3f2d20ad195d15ee9ba4b6671b6037626d
desaipranali37 <desaipranali37@gmail.com>
2025-04-03 13:01:12 +0530

    second

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { CartProvider } from './context/CartContext.jsx'
 import { AuthProvider } from './Context/AuthContext.jsx'
+import { CartProvider } from './Context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
